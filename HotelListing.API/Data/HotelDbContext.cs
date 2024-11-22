3eb7387a85111f1a70d86a214556cb752217236f
Paulo <player_one_117@outlook.com>
2024-11-22 17:27:09 -0500

    Creating Models for Hotel and Country, declaring models in the database context, seeding the models Hotel and Country, making migrations

--- HotelListing.API/Data/HotelDbContext.cs
+++ HotelListing.API/Data/HotelDbContext.cs
@@ -8,5 +8,58 @@ namespace HotelListing.API.Data
         {
             
         }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Brazil",
+                    ShortName = "Bz"
+                },new Country
+                {
+                    Id = 2,
+                    Name = "Mexico",
+                    ShortName = "Mx"
+                },new Country
+                {
+                    Id = 3,
+                    Name = "Japan",
+                    ShortName = "Jp"
+                }
+             );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "The Rolnaldhino",
+                    Address = "Sao Paolo",
+                    Rating = 4.5,
+                    CountryId = 1
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Hinata",
+                    Address = "Kyoto",
+                    Rating = 4.3,
+                    CountryId = 3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Xcaret",
+                    Address = "Riviera Maya",
+                    Rating = 4.7,
+                    CountryId = 2
+                }
+                );
+        }
     }
 }
